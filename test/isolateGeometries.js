25b28a80a589662ab1a3f54bf97f76f1b84df2db
Eyal Soha <eyalsoha@gmail.com>
2017-09-15 22:56:59 +0300

    add unit tests for isolatedGeometries

---
+++ test/isolateGeometries.js
@@ -0,0 +1,28 @@
+import { BufferGeometryAnalyzer, STLLoader } from '..';
+import { expect } from 'chai';
+import fs from 'fs';
+
+describe("isolatedGeometries", function() {
+    let testFile = function (filename, expectedGeometriesCount) {
+        let stl = fs.readFileSync("test/" + filename, {encoding: "ascii"});
+        let geometry = new STLLoader().parse(stl);
+        let newGeometries = BufferGeometryAnalyzer.isolatedGeometries(geometry);
+        expect(newGeometries.length).to.equal(expectedGeometriesCount);
+    }
+
+    it("Simple tetrahedron", function() {
+        testFile("tetrahedron.stl", 1);
+    });
+
+    it("Split ruler with degenerate facets", function() {
+        testFile("lungo.stl", 2);
+    });
+
+    it("2 tetrahedrons that share a face", function() {
+        testFile("face_connected_tetrahedrons.stl", 2);
+    });
+
+    it("2 tetrahedrons that share an edge", function() {
+        testFile("edge_connected_tetrahedrons.stl", 2);
+    });
+});
